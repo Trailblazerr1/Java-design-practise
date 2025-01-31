e4259360fbcc71dab9f87d80188a172b17c575b0
Trailblazerr1 <kanurag078@gmail.com>
2025-01-31 16:28:16 +0530

    Default functional interfaces in java

--- src/com/java/FunctionalInterfaceImpl.java
+++ src/com/java/FunctionalInterfaceImpl.java
@@ -1,4 +1,10 @@
 package com.java;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
 interface FunctionalInterfaceExample {
     abstract void callMe();
     static void staticMethod() {
@@ -28,5 +34,15 @@ class LambdaImpl {
         oldest.callMe();
         older.callMe();
         old.callMe();
+        StringBuilder sb = new StringBuilder("Hello Consumer");
+        Consumer<StringBuilder> consumer = builder ->  System.out.println(sb.toString());
+        Supplier<String> producer = () -> "Hello from producer";
+        Function<Integer,Integer> square = no -> no*no;
+        Predicate<Integer> testEven  = no -> no % 2 == 0;
+
+        consumer.accept(sb);
+        System.out.println(producer.get());
+        System.out.println(square.apply(2));
+        System.out.println(testEven.test(2));
     }
 }
